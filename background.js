acf1b0fc2600e18671c9d3252e582102589b1283
kazuooooo <matsumotokazuya7@gmail.com>
2015-12-16 01:33:44 +0900

    comment add

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
 $(function(){
-  alert("hello")
+  //リンクがクリックされた
   $('a').mousedown(function(){
-  alert($(this).attr('href'));
+    //alert($(this).attr('href'));
+    //タブの状態を取得（多分別スクリプト)
+    //pinned が使える　https://developer.chrome.com/extensions/tabs#type-Tab
+    var isTabFixed = true;
+    if(isTabFixed){
+      alert("tab fixed")
+      //タブが固定されていれば新しいタブを作って遷移
+      //createTab(url);
+    }
   });
 });
